818f4cc8a2f8897d3dd0ddccfc2c5326e2f11107
OGFris <ilyesou.cherfaoui.18@gmail.com>
2020-05-10 08:25:53 +0200

    Updated .gitignore.

---
+++ src/main/java/galacy/galacyhcf/GalacyHCF.java
@@ -0,0 +1,11 @@
+package galacy.galacyhcf;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class GalacyHCF extends PluginBase {
+
+    @Override
+    public void onEnable() {
+        getServer().getLogger().info("this has been loaded bro");
+    }
+}
